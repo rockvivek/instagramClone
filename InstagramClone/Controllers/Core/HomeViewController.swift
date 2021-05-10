3c5b1041b0cb51022be7a73bc3bca64bcaa74be8
vivek shrivastwa <vivekshrivastwa@viveks-MacBook-Air.local>
2021-05-10 14:35:11 +0530

    structure created

---
+++ InstagramClone/Controllers/Core/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by vivek shrivastwa on 10/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
